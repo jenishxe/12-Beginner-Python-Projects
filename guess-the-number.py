182a051093900a626ee74ea816fb11975559066e
Jenish <jenish.codes@gmail.com>
2025-01-01 22:20:58 +0530

    compleated guess numbers program in py

--- guess-the-number.py
+++ guess-the-number.py
@@ -11,4 +11,27 @@ def guess(x):
             print("Sorry guess again, You enter too high")
     print(f'Congrats you guessed the number {random_number}')
 
-guess(10)
+# guess(10)
+
+
+
+# let computer guess your secret number
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f"Is {guess} too High(H), too low(L) or correct (C) : ".lower())
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess +1
+    print(f'Yay computer guessed the number {guess} correctly ')
+
+
+# computer_guess(10000)
